83cebf3adeb42b379032e86203988145885c5289
Jani Tammi <jani@debian-vm.home.net>
2018-11-11 13:42:42 +0200

    Sample import renamed and import now into 'pulseheight' table

--- util/import_sample_pulseheight_data.py
+++ util/import_sample_pulseheight_data.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 # PATE Monitor / Development Utility 2018
 #
-# csv2sqlite.py - Jani Tammi <jasata@utu.fi>
+# import_sample_pulseheight_data.py - Jani Tammi <jasata@utu.fi>
 #
 #   0.1.0   2018.10.11  Initial version.
 #   0.1.1   2018.10.14  Cleaned for GitHub
+#   0.2.0   2018.11.11  Renamed. Saves to 'pulseheight' now.
 #
 #       This utility imports the provided pulse height data sample into a
 #       SQLite database - to be made available via Flask REST API.
@@ -20,9 +21,10 @@ import sqlite3
 
 class Config:
     csv_file     = "sample.csv"
-    db_file      = "pmapi.sqlite3"
+    db_file      = "../pmapi.sqlite3"
     row_first    = time.time()      # Note: Python timestamp has microseconds (float)
     row_interval = 15               # in seconds
+
 
 class excel_finnish(csv.Dialect):
     """Describe the properties of Finnish locale Excel-generated CSV files."""
@@ -32,56 +34,73 @@ class excel_finnish(csv.Dialect):
     skipinitialspace = False
     lineterminator = '\r\n'
     quoting = csv.QUOTE_MINIMAL
+
 csv.register_dialect("excel-finnish", excel_finnish)
+
+
+def get_session(cursor):
+    """Get or create session. Also creates PATE, if needed."""
+    #
+    # Get session, if available
+    #
+    row = cursor.execute("SELECT id FROM testing_session LIMIT 1").fetchone()
+    if row:
+        return row[0]
+    #
+    # No testing_session rows, create one
+    #
+    row = cursor.execute("SELECT id FROM pate LIMIT 1").fetchone()
+    if not row:
+        # create pate
+        cursor.execute("INSERT INTO pate (id_min, id_max, label) VALUES (0, 1000, 'Created to insert sample pulseheight data')")
+        pate_id = cursor.lastrowid
+    else:
+        pate_id = row[0]
+    # create testing_session
+    cursor.execute(
+        """
+        INSERT INTO testing_session (started, pate_id, pate_firmware)
+        VALUES (?, ?, 'Created to insert sample pulseheight data')
+        """,
+        (time.strftime('%Y-%m-%d %H:%M:%S'), pate_id)
+    )
+    return cursor.lastrowid
+
+
 
 if __name__ == '__main__':
 
-    conn = sqlite3.connect(Config.db_file)
-    curs = conn.cursor()
+    from pathlib import Path
 
-    # Create table (or fail gracefully, if already exists)
-    sql_create_table = """
-        CREATE TABLE IF NOT EXISTS sample_pulse_height_data (
-            timestamp   INTEGER,
-            hits        INTEGER,
-            AC1         INTEGER,
-            D1A         INTEGER,
-            D1B         INTEGER,
-            D1C         INTEGER,
-            D2A         INTEGER,
-            D2B         INTEGER,
-            D3          INTEGER,
-            AC2         INTEGER
-        )
-    """
-    try:
-        curs.execute(sql_create_table)
-    except sqlite3.Error as e:
-        print(e)
+    dbfile = Path(Config.db_file)
+    if not dbfile.is_file():
+        print(Config.db_file, "not found!")
         os._exit(-1)
 
-    sql_delete = """
-        DELETE FROM sample_pulse_height_data
-    """
+    connection = sqlite3.connect(Config.db_file)
+    cursor = connection.cursor()
+
+    # First clear out existing data
     try:
-        curs.execute(sql_delete)
+        cursor.execute("DELETE FROM pulseheight")
     except sqlite3.Error as e:
-        print(e)
+        print(str(e))
         os._exit(-1)
 
+    session_id = get_session(cursor)
 
-    sql_insert = """
-        INSERT INTO sample_pulse_height_data (
+    sql = """
+        INSERT INTO pulseheight (
             timestamp,
-            hits,
-            AC1,
-            D1A,
-            D1B,
-            D1C,
-            D2A,
-            D2B,
-            D3,
-            AC2
+            session_id,
+            ac1,
+            d1a,
+            d1b,
+            d1c,
+            d2a,
+            d2b,
+            d3,
+            ac2
         )
         VALUES (
             ?,
@@ -111,11 +130,11 @@ if __name__ == '__main__':
             # import pdb; pdb.set_trace()
             content = list(int(row[i], 2) if i == 20 else int(row[i]) for i in included_cols)
 
-            curs.execute(
-                sql_insert,
+            cursor.execute(
+                sql,
                 (
                     int(Config.row_first + index * Config.row_interval),
-                    content[0],
+                    session_id,
                     content[1],
                     content[2],
                     content[3],
@@ -126,6 +145,6 @@ if __name__ == '__main__':
                     content[8]
                 )
             )
-    conn.commit()
+    connection.commit()
 
 # EOF
