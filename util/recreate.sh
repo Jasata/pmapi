d50294b763c015a73b0d1742601ac24437cb5c65
root <root@rpi31.home.net>
2018-11-26 10:57:23 +0000

    Remove util/ directory

--- util/recreate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 create_database.py
-python3 generate_pate_and_session.py
-python3 generate_hitcount.py 5760
-python3 generate_housekeeping.py 300
-python3 import_sample_pulseheight_data.py
-
